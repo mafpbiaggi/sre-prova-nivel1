93f1c8c0ad37d89a7a5bf232f9ed4f70517dd7c2
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-04-16 18:17:14 -0300

    Update: Atualiza testes automatizados

--- tests/test_app.py
+++ tests/test_app.py
@@ -31,10 +31,12 @@ def test_health_endpoint(client):
 
 def test_request_counter(client):
     """Testa se o contador de requisições funciona"""
-    response1 = client.get('/')
+    client.get('/')
+    response1 = client.get('/metrics')
     count1 = response1.get_json()['total_requests']
-
-    response2 = client.get('/')
+    
+    client.get('/')
+    response2 = client.get('/metrics')
     count2 = response2.get_json()['total_requests']
 
     assert count2 > count1
